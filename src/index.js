536a3d1f128c49725b5414ef764b2091b5c9f71f
omkarSw2 <omkar21143@gmail.com>
2023-11-20 13:34:37 +0530

    connection to mongo

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const { ConnectDB } = require("./db");
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  try {
+    res.status(200).send({ msg: "Success to get the data" });
+  } catch (error) {
+    res.status(400).send({ msg: "Error while geting the data.", error });
+  }
+});
+
+app.listen(process.env.PORT||8080, async () => {
+  try {
+    await ConnectDB();
+    console.log("Connected App ");
+  } catch (error) {
+    console.log("Connection Error on Express App ", error);
+  }
+});
